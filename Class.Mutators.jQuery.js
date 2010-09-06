dab0a0a6c7624002313cacc79407a894d1c0227f
John Bender <john.m.bender@gmail.com>
2010-09-05 23:50:14 -0700

    instantiates many objects for each dom element returned for a selector, whitespace fixes, first argument is jquery object instead of selector

--- Class.Mutators.jQuery.js
+++ Class.Mutators.jQuery.js
@@ -1,39 +1,48 @@
 /*
----
+  ---
 
-script: Class.Mutators.jQuery
+  script: Class.Mutators.jQuery
 
-description: Extends the jQuery object with a MooTools Class
+  description: Extends the jQuery object with a MooTools Class
 
-license: MIT-style license.
+  license: MIT-style license.
 
-authors: Ryan Florence, Christoph Pojer, Ibolmo Maldonado
+  authors: Ryan Florence, Christoph Pojer, Ibolmo Maldonado
 
-requires:
-- core:1.2.4: [Native, Class, Class.Extras]
+  requires:
+  - core:1.2.4: [Native, Class, Class.Extras]
 
-provides: [Class.Mutators.jQuery]
+  provides: [Class.Mutators.jQuery]
 
-...
+  ...
 */
 
 Class.Mutators.jQuery = function(name){
-	var self = this;
-	jQuery.fn[name] = function(arg){
-		var instance = this.data(name);
-		if ($type(arg) == 'string'){
-			var prop = instance[arg];
-			if ($type(prop) == 'function'){
-				var returns = prop.apply(instance, Array.slice(arguments, 1));
-				return (returns == instance) ? this : returns;
-			} else if (arguments.length == 1){
-				return prop;
-			}
-			instance[arg] = arguments[1];
-		} else {
-			if (instance) return instance;
-			this.data(name, new self(this.selector, arg));
-		}
-		return this;
-	};
+  var self = this;
+  jQuery.fn[name] = function(arg){
+    var instance = this.data(name);
+    if ($type(arg) == 'string'){
+      var prop = instance[arg];
+      if ($type(prop) == 'function'){
+        var returns = prop.apply(instance, Array.slice(arguments, 1));
+        return (returns == instance) ? this : returns;
+      } else if (arguments.length == 1){
+        return prop;
+      }
+      instance[arg] = arguments[1];
+    } else {
+      if (instance) return instance;
+
+      // If the selector returns many results instantiate for each
+      if(this.length > 0){
+        this.each(function(i, e){
+          // Send the jquery object itself since we can use o.selector
+          this.data(name, new self($(e), arg));
+        }.bind(this));
+      } else {
+        this.data(name, new self(this, this, arg));
+      }
+    }
+    return this;
+  };
 };
