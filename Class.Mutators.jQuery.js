c7b6e146669db1915deefa8c264214a7fc6e4257
John Bender <john.m.bender@gmail.com>
2010-09-14 01:09:36 -0700

    broadcast method calls to instantiated objects, does not address return values or current return value code

--- Class.Mutators.jQuery.js
+++ Class.Mutators.jQuery.js
@@ -20,25 +20,39 @@
 Class.Mutators.jQuery = function(name){
   var self = this;
   jQuery.fn[name] = function(arg){
-    var instance = this.data(name);
-    if ($type(arg) == 'string'){
-      var prop = instance[arg];
-      if ($type(prop) == 'function'){
-        var returns = prop.apply(instance, Array.slice(arguments, 1));
-        return (returns == instance) ? this : returns;
-      } else if (arguments.length == 1){
-        return prop;
-      }
-      instance[arg] = arguments[1];
-    } else {
-      if (instance) return instance;
+    var event = 'moo4q_event_' + name,
+        send = function(event, arg){
+          var instance = event.data.instance,
+              property = instance[arg],
+              additional_args = Array.slice(arguments, 2);
 
+          if ($type(property) == 'function'){
+            var returns = propery.apply(instance, additional_args);
+            return (returns == instance) ? this : returns;
+          } else if (arguments.length == 0){
+            return prop;
+          }
+
+          // set the property if nothing else
+          instance[arg] = arguments[1];
+        }.bind(this);
+
+    // if calling a method on the objects broadcast the call
+    if ($type(arg) == 'string'){
+      $(document).trigger(event, arguments);
+    } else { // otherwise instantiate where necessary
+      if (this.data(name)) return this.data(name);
+
+      this.data(name, []);
       // instantiate a new object for each selector match
       this.each(function(i, e){
         // Send the jquery object itself since we can use o.selector
-        this.data(name, new self($(e), arg));
+        var new_instance = new self($(e), arg);
+        $(document).bind(event, { instance: new_instance }, send);
+        this.data(name).push(new_instance);
       }.bind(this));
+
+      return this;
     }
-    return this;
   };
 };
